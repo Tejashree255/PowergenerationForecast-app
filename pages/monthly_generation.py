8ce5fbce279f7cb1bcb852ddd23a968e684bc2e9
Tejashree255 <71812857+Tejashree255@users.noreply.github.com>
2023-04-23 20:09:13 +0530

    Update monthly_generation.py

--- pages/monthly_generation.py
+++ pages/monthly_generation.py
@@ -9,10 +9,10 @@ with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 st.subheader('Monthly Avg power generation Forecasting for upcoming 12 months')
 
-input_file = st.file_uploader("Upload input file", type="csv")
+input_file = st.file_uploader("Upload input file")
 
 dataS='Gen. Date'
-dataW='DATE'
+dataW='WEC Wise Report'
 @st.cache_data
 def convert_df(df):
    return df.to_csv(index=False).encode('utf-8')
@@ -21,7 +21,10 @@ def convert_df(df):
 
 if input_file is not None:
     #data=input_file.getvalue().decode('utf-8').splitlines()
-    data = pd.read_csv(input_file,encoding= 'unicode_escape')
+    data = pd.read_excel(input_file)
+    #print("Columns : ",data.loc[[0]])
+    print("Columns : ",data.columns)
+    
     if dataS in data.columns:
             print('S')
             #data = pd.read_csv(input_file,encoding= 'unicode_escape',parse_dates=['Gen. Date'])
@@ -33,6 +36,7 @@ if input_file is not None:
                 df1=pd.DataFrame({"Gen. Date":i,"Customer Name":0,	"State":0,	"Site":0,	"Section":0,"MW":0,	"Loc. No.":0,	"Gen. (kwh) DAY":0,	"Gen. (kwh) MTD":0,	"Gen. (kwh) YTD":0,	"%PLF DAY":0,	"%PLF MTD":0,	"%PLF YTD":0,	"M/C Avail.%":0,	"GF":0	,"FM":0,	"S":0,	"U":0,	"Gen Hrs.":0,	"Opr Hrs.":0},index=[i])
                 data=data.append(df1)
             data = data.set_index("Gen. Date", drop=False)
+            print(data.info())
             data1=data.resample('M').mean()
             data1 = data1.loc[:, ["Gen. (kwh) DAY"]]#index
             data1 = data1.asfreq("M")
@@ -78,9 +82,28 @@ if input_file is not None:
             image =Image.open('output.png')
             st.image(image)
     elif dataW in data.columns:  
+ 
             print('w')
-            #data['Gen. Date']= pd.to_datetime(data['Gen. Date'])
-            #data = pd.read_csv(input_file,encoding= 'unicode_escape',parse_dates=['DATE'])
+            
+            #data.astype({'GENERATION': 'int64'})
+            
+            data.rename(columns={'WEC Wise Report':'a', 'Unnamed: 1':'b', 'Unnamed: 2':'c', 'Unnamed: 3':'d',
+       'Unnamed: 4':'e', 'Unnamed: 5':'f', 'Unnamed: 6':'g', 'Unnamed: 7':'h', 'Unnamed: 8':'i',
+       'Unnamed: 9':'j', 'Unnamed: 10':'k', 'Unnamed: 11':'l', 'Unnamed: 12':'m',
+       'Unnamed: 13':'n', 'Unnamed: 14':'o', 'Unnamed: 15':'p'},inplace=True)
+           
+            data.rename(columns={'a':data['a'].iloc[0], 'b':data['b'].iloc[0], 'c':data['c'].iloc[0],
+                                 'd':data['d'].iloc[0],
+       'e':data['e'].iloc[0], 'f':data['f'].iloc[0], 'g':data['g'].iloc[0], 'h':data['h'].iloc[0], 'i':data['i'].iloc[0],
+       'j':data['j'].iloc[0], 'k':data['k'].iloc[0], 'l':data['l'].iloc[0], 'm':data['m'].iloc[0],
+       'n':data['n'].iloc[0], 'o':data['o'].iloc[0], 'p':data['p'].iloc[0]},inplace=True)
+            data=data.iloc[1:,:]
+            data.reset_index(drop=True,inplace=True)
+            data['GENERATION'] = data['GENERATION'].astype('int64')
+            data['DATE'] = data['DATE'].astype('datetime64') 
+            print("SEE",data.info())
+            
+        
             data['DATE']=pd.to_datetime(data['DATE'])
             lastdate=data['DATE'].iloc[-1]#24/11/2022
             print(data.tail(1))
@@ -88,10 +111,13 @@ if input_file is not None:
             n=12
             for i in (lastdate + timedelta(n) for n in range(365)):
                 df1=pd.DataFrame({"Sr. No":0,"DATE":i,"WEC":0,"WEC Type":0,"GENERATION":0,"O.Hrs":0,"L.Hrs":0,"MA":0,"CF":0,"GIA":0,"GA":0,"REMARKS":0,""
-                                  "Unnamed: 12":0,"Customer":0, "SITE":0,"STATE":0},index=[i])
+                                  "Unnamed: 12":0,"Customer":'abc', "SITE":0,"STATE":0},index=[i])
                 data=data.append(df1)
             data = data.set_index("DATE", drop=False)
+ 
             data1=data.resample('M').mean()
+            
+        
             data1 = data1.loc[:, ["GENERATION"]]#index
             data1 = data1.asfreq("M")
                  #Training and test set
